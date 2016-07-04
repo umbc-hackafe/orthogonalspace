52bb324a2a4e4ae5ad17ee7c1e35a3e0d7bc5e98
Mark Murnane <mark@hackafe.net>
2016-07-04 03:34:01 -0400

    Added login methods

--- orthogonalspace/__main__.py
+++ orthogonalspace/__main__.py
@@ -16,6 +16,7 @@ Options:
 
 from autobahn.asyncio.wamp import ApplicationSession, ApplicationRunner
 import orthogonalspace
+from autobahn import wamp
 import asyncio
 import docopt
 import logging
@@ -38,10 +39,93 @@ class OrthogonalSpaceComponent(ApplicationSession):
     def onJoin(self, details):
         yield from self.register(self.lobby.list_universes, u'space.orthogonal.lobby.list_universes')
         yield from self.register(self.lobby.list_ships, u'space.orthogonal.lobby.list_ships')
-
+        def echo(msg):
+            return str(msg[::-1])
+        yield from self.register(echo, u'space.orthogonal.echo')
+        results = yield from self.register(self)
+        for res in results:
+            if isinstance(res, wamp.protocol.Registration):
+                LOG.debug("Registered procedure {} successfully".format(res))
+            else:
+                LOG.error("Failed to register procedure {}".format(res))
+        results = yield from self.subscribe(self)
+        for res in results:
+            if isinstance(res, wamp.protocol.Subscription):
+                LOG.debug("Subscribed handler with subscription ID {}".format(res))
+            else:
+                LOG.error("Failed to subscribe handler: {}".format(res))
         while True:
             self.publish(u'space.orthogonal.heartbeat', 'Hi')
             yield from asyncio.sleep(1)
+
+    @wamp.register(u'com.login')
+    def login(self, username, password):
+        session = self.db.Session()
+        try:
+            user = session.query(User).filter(User.name == username).one()
+        except:
+            LOG.error("Could not find username {0}".format(username))
+            session.close()
+            return None
+        if user.login(password):
+            # Success!
+            websessions = session.query(WebSession).filter(WebSession.user_id == user.ID).all()
+            for i in websessions:
+                if (i.timestamp < (datetime.datetime.now() - datetime.timedelta(minutes=int(self.config.extra['session_duration']))) and i.user.name == username):
+                    session.delete(i)
+            websession = WebSession(user.ID)
+            websessionID = websession.uuid
+            session.add(websession)
+            LOG.info("User {0} logged in successfully with session {1}".format(username, websession.uuid))
+            session.commit()
+            session.close()
+            return str(websessionID)
+        else:
+            LOG.info("Login failed for user {0}".format(username))
+            # Failure!
+            session.close()
+            return None
+
+    @wamp.register(u'com.sessioncheck')
+    def sessioncheck(self, username, sessionID):
+        session = self.db.Session()
+        try:
+            websession = session.query(WebSession).filter(WebSession.uuid == sessionID).one()
+        except:
+            LOG.error("Could not find session {0} for user {1}".format(sessionID, username))
+            session.close()
+            return False
+        if (websession.timestamp > (datetime.datetime.now() - datetime.timedelta(minutes=int(self.config.extra['session_duration']))) and websession.user.name == username):
+            LOG.info("Renewing session {0} for user {1}".format(sessionID, username))
+            websession.timestamp = datetime.datetime.now()
+            session.add(websession)
+            session.commit()
+            session.close()
+            return True
+        else:
+            LOG.info("Invalid session {0} attempted for user {1}".format(sessionID, username))
+            session.close()
+            return False
+
+    @wamp.register(u'com.logout')
+    def logout(self, username, sessionID):
+        session = self.db.Session()
+        try:
+            websession = session.query(WebSession).filter(WebSession.uuid == sessionID).one()
+        except:
+            LOG.error("Could not find session {0} for user {1} during logout".format(sessionID, username))
+            session.close()
+            return False
+        if (websession.timestamp > (datetime.datetime.now() - datetime.timedelta(minutes=int(self.config.extra['session_duration']))) and websession.user.name == username):
+            LOG.info("Logging out user {0} from session {1}".format(username, sessionID))
+            session.delete(websession)
+            session.commit()
+            session.close()
+            return True
+        else:
+            LOG.info("User {0} failed to log out with session {1}".format(username, sessionID))
+            session.close()
+            return False
 
 def main():
     # load command-line options
